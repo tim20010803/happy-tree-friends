7e3a2c14241f0ba3bf9736cb95706742b526b0a5
tim20010803 <89471956+tim20010803@users.noreply.github.com>
2023-06-07 02:42:07 +0800

    裝進去

--- compileNonTree_one_step.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++11 quadrupleTree.cpp orbit_integration.cpp mainNonTree_one_step.cpp -o mainNonTree_one_step.out
--- compileTree_one_step.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++11 quadrupleTree.cpp orbit_integration.cpp mainTree_one_step.cpp -o mainTree_one_step.out
